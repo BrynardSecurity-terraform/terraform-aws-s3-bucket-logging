0d26e5f4ce9bd2d6b6b24a549cc942b1097837ee
RalphBrynard <ralph@thebrynards.com>
2022-04-01 11:23:13 -0400

    S3 Bucket refactoring - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-4-upgrade\#s3-bucket-refactor

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "this" {
   bucket = "${var.name_prefix}-logging${var.name_suffix}"
   acl    = "log-delivery-write"
-  
-  versioning {
-    enabled = var.versioning_enabled
-  }
   
   dynamic "replication_configuration" {
     #If conditions are true enable versioning, if they're false do nothing
@@ -59,6 +55,94 @@ resource "aws_s3_bucket" "bucket" {
   }
 
   tags = var.input_tags
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this
+  
+  dynamic "versioning_enabled" {
+    for_each = var.versioning_enabled == true ? [true] : []
+    versioning_configuration {
+      status = "Enabled"
+    }
+  }
+
+  dynamic "versioning_disabled" {
+    for_each = var.versioning_enabled == true ? [] : [true]
+    versioning_configuration {
+      status = "Disabled"
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this
+
+  dynamic "server_side_encryption_configuration"{
+    for_each = var.versioning_enabled == true && var.enable_centralized_logging == true ? [true] : []
+    server_side_encryption_configuration {
+      rule {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_rule" "this" {
+  bucket = aws_s3_bucket.this
+
+  dynamic "aws_s3_bucket_lifecycle_rule" {
+    for_each = var.versioning_enabled == true && var.enable_centralized_logging == true ? [true] : []
+    rule {
+      prevent_destroy = true
+    }
+    rule {
+      id = "Logs"
+      enabled = true
+
+      filter {
+        and {
+          prefix = "/"
+        }
+      }
+
+      transition {
+        noncurrent_days = var.transition_IA
+        storage_class = "STANDARD_ID"
+      }
+
+      transition {
+        noncurrent_days = var.transition_glacier
+        storage_class = "GLACIER"
+      }
+
+      expiration {
+        days = var.transition_expiration
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_replication_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+  role = var.iam_role_s3_replication_arn
+
+  dynamic "aws_s3_bucket_replication_configuration" {
+    for_each = var.versioning_enabled == true && var.enable_centralized_logging == true ? [true] : []
+    rule {
+      id = "${var.name_prefix}-replication${var.name_suffix}"
+      status = "Enabled"
+    }
+
+    destination {
+      bucket = "arn:aws:s3:::${var.s3_destination_bucket_name}"
+      storage_class = var.replication_dest_storage_class
+      account_id = var.logging_account_id
+      access_control_translation {
+        owner = "Destination"
+      }
+    }
+  }
 }
 
 data "aws_elb_service_account" "elb_account" {}
